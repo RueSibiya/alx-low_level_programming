5dcb1dc210277392a5bc6fbbc1f06f0a2e39fe6d
RueSibiya <rksibiya1@gmail.com>
2023-07-15 23:59:14 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main function is the entry point of the program
+ *this program prints a line of text on the screen
+ *this program is betty compliant
+ *return:0(success)
+*/
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
