fd7e73e75e09779cdfe5b9d0418da8663fe601cf
RueSibiya <rksibiya1@gmail.com>
2023-07-16 00:54:58 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+
 /**
- *main function is the entry point of the program
- *this program prints a line of text on the screen
- *this program is betty compliant
- *return:0(success)
+ * main - entry point
+ * description: this program prints a line of text on the screen
+ * this program is betty compliant
+ * Return: 0 (Success)
 */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle);
 	return (0);
 }
