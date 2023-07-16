90ac24f965d3b12128b3773888f0fb8022a1e7b0
RueSibiya <rksibiya1@gmail.com>
2023-07-16 02:45:28 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: this program prints a line of text on the screen
+ * this program is betty compliant
+ * Return: (0) success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
