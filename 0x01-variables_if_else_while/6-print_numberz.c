0d9f2482730d115dba484fdeec179ec3512c7d36
RueSibiya <rksibiya1@gmail.com>
2023-07-18 15:08:16 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+* Return: (0) is always a success
+*/
+
+int main(void)
+
+{
+putchar('0');
+putchar('\n');
+
+putchar('1');
+putchar('\n');
+
+putchar('2');
+putchar('\n');
+
+putchar('3');
+putchar('\n');
+
+putchar('4');
+putchar('\n');
+
+putchar('5');
+putchar('\n');
+
+putchar('6');
+putchar('\n');
+
+putchar('7');
+putchar('\n');
+
+putchar('8');
+putchar('\n');
+
+putchar('9');
+putchar('\n');
+
+return (0);
+}
+
+
+
+
+
+
+
