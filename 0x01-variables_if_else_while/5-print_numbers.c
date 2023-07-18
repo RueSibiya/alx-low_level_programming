625525e767de79517349461cc4129206fdfbf7f9
RueSibiya <rksibiya1@gmail.com>
2023-07-18 14:38:45 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+* Description: this program is Betty compliant
+* Return: Success should always be (0)
+*/
+
+int main(void)
+{
+int a;
+
+for (a = 0; a < 10; a++)
+{
+printf("%d\n", a);
+}
+return (0);
+}
