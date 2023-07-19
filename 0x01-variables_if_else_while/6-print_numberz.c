868ebae3cd2a5688f31821cc12b51b7da0350416
RueSibiya <rksibiya1@gmail.com>
2023-07-19 03:25:22 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,37 +8,17 @@
 int main(void)
 
 {
-putchar('0');
-putchar('\n');
+	int number_start = '0';
+	int number_end = '9';
 
-putchar('1');
-putchar('\n');
+	int i = number_start;
 
-putchar('2');
-putchar('\n');
-
-putchar('3');
-putchar('\n');
-
-putchar('4');
-putchar('\n');
-
-putchar('5');
-putchar('\n');
-
-putchar('6');
-putchar('\n');
-
-putchar('7');
-putchar('\n');
-
-putchar('8');
-putchar('\n');
-
-putchar('9');
-putchar('\n');
-
-return (0);
+	for (; i <= number_end; i++)
+	{
+		putchar(i);
+		putchar('\n');
+	}
+	return (0);
 }
 
 
