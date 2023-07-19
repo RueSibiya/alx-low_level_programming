ddb9fbe2de1d14208781f99d4192e7474d4ef0d0
RueSibiya <rksibiya1@gmail.com>
2023-07-19 04:20:29 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  * main - entry point
+  * Return: always 0 (success)
+  */
+
+int main(void)
+{
+	char tebahpla;
+
+	for (tebahpla = 'z'; tebahpla >= 'a'; tebahpla--)
+
+	{
+		putchar(tebahpla);
+	}
+	printf('\n');
+
+	return (0);
