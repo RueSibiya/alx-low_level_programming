82c6217f62d0a0bc9ad51e4d16d81f7505889f1a
RueSibiya <rksibiya1@gmail.com>
2023-08-18 01:42:04 -0700

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -20,7 +20,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(strings, n);
 
 	for (index = 0; index < n; index++)
-        {
+	{
 
 		str = va_arg(strings, char *);
 
